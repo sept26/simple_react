da9c62abc6c7e7eff8755f0a9dea56ccccdf3b8b
one_xin <wx3364@163.com>
2019-03-13 22:58:47 +0800

    主页样式

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { saveFormData, saveImage, clearData } from '@/store/home/action'
 // import { is, fromJS} from 'immutable'
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types'
 // import API from './../../api/api'
 // import envconfig from './../../envconfig/envconfig';
 import Header from './../../components/header'
+import './home.less'
 
 class Home extends Component {
   constructor(props) {
@@ -28,18 +29,24 @@ class Home extends Component {
         <p className="person-info">请输入您的信息</p>
         <form className="form-group">
           <div>
-            <label>销售金额</label>
+            <label>销售金额:</label>
             <input type="text" placeholder="请输入订单金额" value={this.props.formData.orderSum} onChange={this.handleInput.bind(this, 'orderSum')}/>
           </div>
           <div>
-            <label>客户姓名</label>
-            <input type="text" placeholder="请输入客户姓名"/>
+            <label>客户姓名:</label>
+            <input type="text" placeholder="请输入客户姓名" value={this.props.formData.name} onChange={this.handleInput.bind(this,'name')}/>
           </div>
           <div>
-            <label>客户电话</label>
-            <input type="text" placeholder="请输入客户电话"/>
+            <label>客户电话:</label>
+            <input type="text" placeholder="请输入客户电话" value={this.props.formData.phoneNo} onChange={this.handleInput.bind(this,'phoneNo')}/>
           </div>
         </form>
+        <div className="select-product">
+          <p>请选择销售的产品</p>
+          <Link to="/production" className="common-select">
+            选择产品
+          </Link>
+        </div>
       </div>
     );
   }
